2307269fa9fe834f6b3a608f2e32ca94f266a634
Olga Yarmolyuk <yarmolyuk.o1ga@mail.ru>
2019-10-16 23:41:06 +0500

    Start!

---
+++ app/src/main/java/ui/activities/SignUpActivity.kt
@@ -0,0 +1,13 @@
+package ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import bonch.dev.school.R
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
