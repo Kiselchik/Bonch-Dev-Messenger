1f49be8837eb7bd2f9017a064272b71e779f2376
Olga Yarmolyuk <yarmolyuk.o1ga@mail.ru>
2019-10-24 23:18:23 +0500

    master

--- app/src/main/java/bonch/dev/school/ui/activities/SignInActivity.kt
+++ app/src/main/java/bonch/dev/school/ui/activities/SignInActivity.kt
@@ -1,13 +1,43 @@
 package bonch.dev.school.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import bonch.dev.school.R
 
 class SignInActivity : AppCompatActivity() {
 
+
+    private lateinit var signInButton: Button
+    private lateinit var signUpButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        setContentView(R.layout.sign_in_activity)
+        initializeView()
+        setListener()
     }
+
+    private fun initializeView(){
+        signInButton = findViewById(R.id.sign_in_button)
+        signUpButton = findViewById(R.id.sign_up_button)
+    }
+
+
+    private fun setListener(){
+
+        signInButton.setOnClickListener{
+            val intent = Intent(this@SignInActivity,MainAppActivity::class.java)
+            startActivity(intent)
+
+        }
+
+        signUpButton.setOnClickListener{
+            val intent = Intent(this@SignInActivity, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+
 }
